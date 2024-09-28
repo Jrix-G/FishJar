69b09dc33248b35d1f13bb1e5e10518ec0668108
Jason <jrixweb@gmail.com>
2024-09-28 18:04:24 +0200

    +

--- fishjar/src/Pages/test.py
+++ fishjar/src/Pages/test.py
@@ -1,7 +1,36 @@
-n = int(input("value: "))
-allpair = True
-while n!=0:
-    if n%2!=0:
-        allpair = False
-    n = int(input("value: "))
-print(allpair)
+# Ecrivez votre programme ci-dessous.
+# Bouton Fullscreen pour passer en plein ecran
+# Ensuite Save + Run puis Save + Evaluate
+# Pour des raisons techniques, laissez une ligne blanche avant de commencer votre programme.
+
+
+def stud():
+    numbers = []
+    while True:
+        num = float(input("Nombre ? "))
+        if num == 0:
+            break
+        numbers.append(num)
+    
+    if not numbers: 
+        print("Seulement 0")
+        return
+    
+    all_positive = all(n > 0 for n in numbers)
+    all_negative = all(n < 0 for n in numbers)
+    
+    if all_positive:
+        print("Tous +")
+    elif all_negative:
+        print("Tous -")
+    else:
+        print("Ni tous +, ni tous -")
+        total_sum = sum(numbers)
+        if total_sum > 0:
+            print("Somme +")
+        elif total_sum < 0:
+            print("Somme -")
+        else:
+            print("Somme = 0")
+
+stud()
