945a49c89f2789902430881cd2210be8e8aa2e8a
Jason <jrixweb@gmail.com>
2024-09-29 01:03:09 +0200

    +

--- fishjar/src/Pages/test.py
+++ fishjar/src/Pages/test.py
@@ -1,9 +1,5 @@
 # Ecrivez votre programme ci-dessous.
 # Bouton Fullscreen pour passer en plein ecran
-# Ensuite Save + Run puis Save + Evaluate
-# Pour des raisons techniques, laissez une ligne blanche avant de commencer votre programme.
-
-
 def stud():
     numbers = []
     while True:
