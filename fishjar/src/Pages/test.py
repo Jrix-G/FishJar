a4e3914b3d93b473030dfee641947cf99913a284
Jason <jrixweb@gmail.com>
2024-10-28 17:08:02 +0100

    +

--- fishjar/src/Pages/test.py
+++ fishjar/src/Pages/test.py
@@ -1,33 +1,23 @@
-# Ecrivez votre programme ci-dessous.
-# Bouton Fullscreen pour passer en plein ecran
-def stud():
-    numbers = []
-    while True:
-        num = float(input("Nombre ? "))
-        if num == 0:
-            break
-        numbers.append(num)
-    
-    if not numbers: 
-        print("Seulement 0")
+def sommeDivPropre(n):
+    if n <= 1:
+        return 0
+    somme = 0
+    for i in range(1, n // 2 + 1):
+        if n % i == 0:
+            somme += i
+    return somme
+
+def estParfait(n):
+    return sommeDivPropre(n) == n
+
+def parfaits_entre(binf=2, bsup=100):
+    if binf > bsup:
+        print("Test avec bmin > bsup")
         return
     
-    all_positive = all(n > 0 for n in numbers)
-    all_negative = all(n < 0 for n in numbers)
-    
-    if all_positive:
-        print("Tous +")
-    elif all_negative:
-        print("Tous -")
+    print(f"Nombres parfaits de [{binf},{bsup}]")
+    parfaits = [n for n in range(binf, bsup + 1) if estParfait(n)]
+    if parfaits:
+        print(" ".join(map(str, parfaits)))
     else:
-        print("Ni tous +, ni tous -")
-        total_sum = sum(numbers)
-        if total_sum > 0:
-            print("Somme +")
-        elif total_sum < 0:
-            print("Somme -")
-        else:
-            print("Somme = 0")
-
-stud()
-
+        print("Aucun nombre parfait trouvé dans cet intervalle.")
