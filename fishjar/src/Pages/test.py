b5266118a87dcdabd625ceb4684f25f03092a43e
Jason <jrixweb@gmail.com>
2024-11-09 11:20:41 +0100

    +

--- fishjar/src/Pages/test.py
+++ fishjar/src/Pages/test.py
@@ -18,8 +18,3 @@ def sommeDivPropre(n):
         if n % i == 0:
  def sommeDivPropre(n):
     if n <= 1:
-        return 0
-    somme = 0
-    for i in range(1, n // 2 + 1):
-        if n % i == 0:
- def sommeDivPropre(n):
